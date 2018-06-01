283c6db7b03c832ae5f1f0c3356ce50e8623aff1
goodlygooder <39072369+goodlygooder@users.noreply.github.com>
2018-06-01 12:19:01 -0700

    Add files via upload

--- Game/myGame/js/main.js
+++ Game/myGame/js/main.js
@@ -1,33 +1,36 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO); //make game
 var flipFlop = true; //global variable that's just used as a toggle
-var lighter;
-var player;
-var nameLabel;
-var instructions;
-var light;
-var lightLevel;
-var hole;
-var timeMultiplier;
+//var lighter;
+//var player;
+//var hand;
+//var nameLabel;
+//var instructions;
+//var light;
+//var lightLevel;
+//var hole;
+//var timeMultiplier;
 var keysPressed = [false, false, false, false, false, false, false, false, false, false, 
 					false, false, false, false, false, false, false, false, false, false, 
 					false, false, false, false, false];
 
-var currentPrompt= [false, false, false, false, false, false, false, false, false, false, 
+var currentPrompt = [false, false, false, false, false, false, false, false, false, false, 
 					false, false, false, false, false, false, false, false, false, false, 
 					false, false, false, false, false];
-var passed = false;
+//var passed = false;
 
-var openLighter;
-var strike;
+//var openLighter;
+//var strike;
 var theme;
+var hands;
+var hand;
 
 
 var Menu = function(game) {};
 Menu.prototype = {
     preload: function() {
         console.log("Menu: preload");
-        game.load.atlas('lighter', 'assets/img/lighterAnimation.png', 'assets/img/lighterAnimation.json');
+        game.load.atlas('lighter', 'assets/img/lighter.png', 'assets/img/lighter.json');
         game.load.audio('theme', ['assets/audio/417993__magmusas__creepy-bell-music-1.mp3', 'assets/audio/417993__magmusas__creepy-bell-music-1.ogg'] );
         //https://freesound.org/people/ABStudios/sounds/177157/ changed by cutting out sections as needed
 		game.load.audio('openLighter', ['assets/audio/lighterOpen.mp3', 'assets/audio/lighterOpen.ogg'] );
@@ -43,17 +46,20 @@ Menu.prototype = {
         timedEvent = qTimer.add(Phaser.Timer.SECOND * 5, this.timerEnd, this);
         qTimer.start();
 
-        game.add.text(100, 100, 'Menu',{font: '50px Courier', fill: '#ffffff'});
+        //game.add.text(100, 100, 'Menu',{font: '50px Courier', fill: '#ffffff'});
         lighter = game.add.sprite(300, 150, 'lighter');
-        lighter.animations.add('idle', ['lighter.png'], 1, true);
-        lighter.animations.add('fire', ['lighter1.png', 'lighter2.png'], 5, true);
-        game.add.text(500, 400, 'Hold q',{font: '50px Courier', fill: '#ffffff'});
+        lighter.animations.add('idle', ['lighter0.png'], 1, true);
+        lighter.animations.add('fire', ['lighter1.png', 'lighter2.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png','lighter3.png', 'lighter4.png'], 5, true);
+        //game.add.text(300, 425, 'Hold q',{font: '50px Courier', fill: '#ffffff'});
+        game.add.text(250, 200, 'Q',{font: '50px Courier', fill: '#ffffff'});
 
         openLighter = game.add.audio('openLighter');
         strike = game.add.audio('strike');
         theme = game.add.audio('theme');
 
     },
+
+
 
     update: function() {
 
@@ -79,7 +85,7 @@ Menu.prototype = {
         //if q is pressed, initiate the fire
         if(game.input.keyboard.isDown(Phaser.Keyboard.Q) == true){
             lighter.animations.play('fire');
-            game.add.text(200, 500, 'Never let go of q',{font: '50px Courier', fill: '#ffffff'});
+            //game.add.text(150, 500, 'Never let go of q',{font: '50px Courier', fill: '#ffffff'});
             
         }
         else{
@@ -92,10 +98,83 @@ Menu.prototype = {
     timerEnd:  function()
     {
     	console.log('q timer finished');
-    	game.state.start('GamePlay');
+    	game.state.start('Instructions');
     }
 
 
+}
+
+var Instructions = function(game) {};
+Instructions.prototype = {
+    preload: function() {
+        console.log("Instructions: preload");
+        game.load.atlas('player1', 'assets/img/walk.png', 'assets/img/walk.json');
+        game.load.image('shack1', 'assets/img/shack.png');
+        game.load.image('shack2', 'assets/img/shack-open.png');
+        //game.load.image('standing', 'assets/img/standingPlayer.png');
+    },
+
+    create: function() {
+        shack = game.add.sprite(0, 0, 'shack1');
+        
+        player1 = game.add.sprite(300, 130, 'player1');
+        player1.scale.setTo(1.2, 1.2);
+        player1.animations.add('walk1', ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'], 6, true);
+        game.physics.enable([player1], Phaser.Physics.ARCADE);
+
+        hitBox = game.add.sprite(700, 130, 'player1');
+        game.physics.enable([hitBox], Phaser.Physics.ARCADE);
+        hitBox.alpha = 0;
+
+        qTimer = game.time.create(false);
+       	leewayTimer = qTimer.add(Phaser.Timer.SECOND * .25, this.leewayEnd, this);
+       	qTimer.start();
+       	flipFlop = false;
+       	console.log('Leeway timer start');
+
+       	shack.inputEnabled = true;
+       	shack.events.onInputDown.add(this.openDoor, this);
+
+
+        //player1.body.collideWorldBounds=true;
+    },
+
+    update: function() {
+        if((!game.input.keyboard.isDown(Phaser.Keyboard.Q) && flipFlop))
+        {
+            theme.stop();
+            game.state.start('GameOver');
+        }
+
+        game.physics.arcade.overlap(player1, hitBox, function play(){game.state.start('GamePlay');}, null, this);
+
+    //     if(game.input.keyboard.isDown(Phaser.Keyboard.W)){
+    //         player1.animations.play('walk1');
+    //         player1.x += 3;
+    //         //game.debug.body(player1);
+
+    //     }
+        
+
+    //     if(!game.input.keyboard.isDown(Phaser.Keyboard.W)){
+    //         player1.animations.stop('walk1');
+    //     }
+    },
+
+    leewayEnd:  function()
+    {
+    	console.log('Leeway timer end');
+    	flipFlop = true;
+    },
+
+    openDoor: function()
+    {
+    	shack.loadTexture('shack2', 0);
+    	player1.body.velocity.x = 100;
+    	player1.animations.play('walk1');
+
+
+    },
 }
 
 var GamePlay = function() {};
@@ -103,13 +182,17 @@ GamePlay.prototype = {
      preload: function() {
         console.log("GamePlay: preload");
         game.load.atlas('player', 'assets/img/walk.png', 'assets/img/walk.json');
+        game.load.atlas('hand1', 'assets/img/creepyHands1.png', 'assets/img/creepyHands1.json' );
+        game.load.atlas('hand2', 'assets/img/creepyHands2.png', 'assets/img/creepyHands2.json' );
+        game.load.atlas('keys', 'assets/img/keys.png', 'assets/img/keys.json' );
+        game.load.atlas('hole', 'assets/img/hole.png', 'assets/img/hole.json'); //light hole around player
         game.load.image('background', 'assets/img/background.png'); //blue background
         game.load.image('background0', 'assets/img/background0.png');//ground
         game.load.image('background1', 'assets/img/background1.png');//closest tree
         game.load.image('background2', 'assets/img/background2.png');
         game.load.image('background3', 'assets/img/background3.png');
         game.load.image('background4', 'assets/img/background4.png');//farthest tree
-        game.load.image('hole', 'assets/img/hole1.png'); //light hole around player
+        
     },
 
     create: function() {
@@ -127,12 +210,19 @@ GamePlay.prototype = {
        		promptTimer.start();
 
        		timeMult = game.time.create(false);
-       		timeMult.loop(Phaser.Timer.SECOND * 5, function timeAdvance(){ if(timeMultiplier < 2) {timeMultiplier += .05;}}, this);
+       		timeMult.loop(Phaser.Timer.SECOND * 5, function timeAdvance(){ if(timeMultiplier < 1.9) {timeMultiplier += .05;}}, this);
        		timeMult.start();
 
        		lightTimer = game.time.create(false);
        		lightTimer.loop(Phaser.Timer.SECOND, this.lowerLight, this);
        		lightTimer.start();
+
+       		handSpawn = game.time.create(false);
+       		handSpawn.loop(Phaser.Timer.SECOND * 5, this.handSpawn, this);
+       		handSpawn.start();
+
+       		
+
 
        		console.log('promptTimer start');
             //Gameplay text
@@ -145,16 +235,42 @@ GamePlay.prototype = {
             background3 = game.add.tileSprite(0, -50, 800, 600, 'background3');
             background2 = game.add.tileSprite(0, -50, 800, 600, 'background2');
             background1 = game.add.tileSprite(0, -50, 800, 600, 'background1');
+
+            keys = [game.add.sprite(350,280, 'keys'), game.add.sprite(400,280, 'keys'), game.add.sprite(450,280, 'keys')];
+            for(var i = 0; i < keys.length; i ++)
+            {
+            	keys[i].animations.add('display', ['A.png', 'B.png', 'C.png', 'D.png', 'E.png', 'F.png', 'G.png', 'H.png', 'I.png', 'J.png', 'K.png', 'L.png', 'M.png', 'N.png', 'O.png', 'P.png', 'R.png', 'S.png', 'T.png', 'U.png', 'V.png', 'W.png', 'X.png', 'Y.png', 'Z.png'], 5, true);
+            	keys[i].frame = 0;
+            	keys[i].visible = false;
+            }
+
+
             player = game.add.sprite(350, 320, 'player');
+            game.physics.arcade.enable(player);
+            //player.anchor.setTo(.5, .5);
             player.animations.add('walk', ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'], 6, true);
+
+            hands = game.add.group();
+            //game.physics.arcade.enable(hands);
+            hands.enableBody = true;
+
+            
+
             hole = game.add.sprite(0, 0, 'hole');
+            hole.animations.add('lightness', ['hole1.png', 'hole2.png', 'hole3.png', 'hole4.png', 'hole5.png', 'hole6.png', 'hole7.png', 'hole8.png', 'hole9.png', 'hole10.png', 'hole11.png', 'hole12.png', 'hole13.png', 'hole14.png', 'hole15.png', 'hole16.png'], 6, true);
+            ui = game.add.group();
+            ui.add(hole);
+            ui.add(keys[0]);
+            ui.add(keys[1]);
+            ui.add(keys[2]);
             nameLabel = game.add.text(100, 100, '',{font: '50px Courier', fill: '#ffffff'});
             instructions = game.add.text(400, 200, 'Press keys when prompted',{font: '25px Courier', fill: '#ffffff'});
             light = 50;
             timeMultiplier = 1;
+            passed = false;
             lightLevel = game.add.text(100,175, 'Light: ' + light, {font: '20px Courier', fill: '#ffffff'} );
             theme.loopFull();
-
+            player.animations.play('walk');
 
     },
 
@@ -176,15 +292,24 @@ GamePlay.prototype = {
     	{
     		checkKeyInput();
     	}
+
+    	game.physics.arcade.overlap(player, hands, this.handCatch, null, this);
+    	this.updateLight();
     	
     	
         //move character
-        player.animations.play('walk');
+        
+        //hand.animations.play('creep');
 
         background1.tilePosition.x -= 1.25;
         background2.tilePosition.x -= 1;
         background3.tilePosition.x -= 0.75;
         background4.tilePosition.x -= 0.5;
+        hole.sendToBack();
+
+
+       // game.debug.body(player);
+       // game.debug.body(hands);
     },
 
     leewayEnd:  function()
@@ -197,6 +322,89 @@ GamePlay.prototype = {
     {
     	light -= 1;
     	lightLevel.text = 'Light: ' + light;
+    },
+
+    handSpawn: function()
+    {
+    	if(game.rnd.integerInRange(0,1) == 0)
+    	{
+    		hand = hands.create(game.rnd.integerInRange(-800,1600), -200, 'hand1');
+        	hand.animations.add('creep', ['creepyHands1.0.png', 'creepyHands1.1.png', 'creepyHands1.2.png'], 4, true);
+    	}
+    	else
+    	{
+    		hand = hands.create(game.rnd.integerInRange(-800,1600), -200, 'hand2');
+        	hand.animations.add('creep', ['creepyHands2.0.png', 'creepyHands2.1.png', 'creepyHands2.2.png'], 4, true);
+    	}
+    	
+        hand.animations.play('creep');
+        game.physics.arcade.enable(hand);
+        hand.body.setSize(100,100, 0, 200);
+        hand.anchor.setTo(.5, .5);
+        hand.rotation = game.physics.arcade.angleToXY(hand, 350 + player.width * .75, 320 + player.height * .75) + Math.PI/2;
+        game.physics.arcade.moveToXY(hand, 350 + player.width * .75, 320 + player.height * .75, 5, 5000);
+        hand.inputEnabled = true;
+        hand.events.onInputDown.add(this.handClick, this);
+    },
+
+    handClick: function()
+    {
+    	//hand.destroy();
+    	//catchTime.destroy();
+    	//hand.setVelocity(-hand.body.velocity.x, -hand.body.velocity.y);
+    	hand.body.velocity.x = -hand.body.velocity.x;
+    	hand.body.velocity.y = -hand.body.velocity.y;
+    	hand.inputEnabled = false;
+    },
+
+    handCatch: function()
+    {
+    	console.log('catch');
+    	hand.destroy();
+    	// catchTime.destroy();
+    	light -= 15;
+    	lightLevel.text = 'Light: ' + light;
+    },
+
+    updateLight: function()
+    {
+    	console.log('update Light' + light);
+    	if(light > 45)
+    	{
+    		hole.frame = 3
+    	}
+    	else if(light > 40)
+    	{
+    		hole.frame = 4;
+    	}
+    	else if(light > 35)
+    	{
+    		hole.frame = 5;
+    	}
+    	else if (light > 30)
+    	{
+    		hole.frame = 6;
+    	}
+    	else if(light > 25)
+    	{
+    		hole.frame = 7;
+    	}
+    	else if(light > 20)
+    	{
+    		hole.frame = 8;
+    	}
+    	else if(light > 15)
+    	{
+    		hole.frame = 9;
+    	}
+    	else if(light > 10)
+    	{
+    		hole.frame = 10;
+    	}
+    	else
+    	{
+    		hole.frame = 11;
+    	}
     },
 
 
@@ -234,11 +442,22 @@ GamePlay.prototype = {
 	    			{
 	    				message += String.fromCharCode(i + 98);
 	    			}
-	    			
+	    			keys[j].frame = i;
+	    			keys[j].visible = true
 	    		}
 	    		
     		}
+
     	}
+    	if(num < 3)
+    	{
+    		keys[2].visible = false;
+    	}
+    	if(num < 2)
+    	{
+    		keys[1].visible = false;
+    	}
+    	
 
     	
 
@@ -412,6 +631,10 @@ checkKeyInput = function()
     		if(wasPrompt)
     		{
     			nameLabel.text = 'passed';
+    			for(var i = 0; i < keys.length; i++)
+    			{
+    				keys[i].visible = false;
+    			}
     		}
     		
     	}
@@ -421,6 +644,7 @@ checkKeyInput = function()
 
 //standard states
 game.state.add('Menu', Menu);
+game.state.add('Instructions', Instructions);
 game.state.add('GamePlay', GamePlay);
 game.state.add('GameOver', GameOver);
 game.state.start('Menu');
